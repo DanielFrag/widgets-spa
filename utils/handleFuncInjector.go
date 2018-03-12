139b402a9b5b7784ec722a1a1e1098b75700f48d
daniel <danielmarcos2@yahoo.com.br>
2018-03-12 14:09:31 -0300

    Ajuste para documentação

--- utils/handleFuncInjector.go
+++ utils/handleFuncInjector.go
@@ -4,13 +4,15 @@ import (
 	"net/http"
 )
 
+//HandlerFuncInjector store the http.HandlerFunc and inject its middlewares
 type HandlerFuncInjector struct {
 	Dependencies []func(http.HandlerFunc) http.HandlerFunc
-	Handler     http.HandlerFunc
+	Handler      http.HandlerFunc
 }
 
+//InjectDependencies inject the middlewares
 func (hfi *HandlerFuncInjector) InjectDependencies() {
-	for i := len(hfi.Dependencies) - 1; i >=0; i-- {
+	for i := len(hfi.Dependencies) - 1; i >= 0; i-- {
 		hfi.Handler = hfi.Dependencies[i](hfi.Handler)
 	}
 }
